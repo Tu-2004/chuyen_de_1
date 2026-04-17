573d58090312ce96d3624aa3579342ffaf5bf72e
Tu-2004 <144471134+Tu-2004@users.noreply.github.com>
2026-04-17 12:50:24 +0700

    Add files via upload

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+<<<<<<< HEAD
+=======
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+>>>>>>> 425f607 (init project student manager)
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+<<<<<<< HEAD
     protected $fillable = [
     'ma_sv',
     'ho_ten',
@@ -14,3 +19,18 @@ class Student extends Model
     'lop'
 ];
 }
+=======
+    use HasFactory;
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    public function subjects()
+    {
+        return $this->belongsToMany(Subject::class)
+            ->withPivot(['score', 'registered_at']);
+    }
+}
+>>>>>>> 425f607 (init project student manager)
